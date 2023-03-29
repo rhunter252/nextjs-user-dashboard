8b4de174f06559f6a68cf33c15fa5b0931b400de
Randy Hunter <randyh252@gmail.com>
2023-03-29 18:26:59 -0400

    add data from faker

--- src/components/TopCards.jsx
+++ src/components/TopCards.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-
+import { faker } from "@faker-js/faker";
+// import { faker } from '@faker-js/faker/locale/en';
 const TopCards = () => {
   return (
     <div className="grid lg:grid-cols-5 gap-4 p-4">
       <div className="col-span-1 lg:col-span-2 bg-white flex justify-between w-full border p-4 rounded-lg">
         <div className="flex flex-col w-full pb-4">
-          <p className="text-2xl font-bold">$7,753</p>
+          <p className="text-2xl font-bold">${faker.finance.amount()}</p>
           <p className="text-gray-600">Daily Revenue</p>
         </div>
         <div className="flex justify-center items-center border rounded-lg bg-green-200 text-green-700 text-lg p-2">
